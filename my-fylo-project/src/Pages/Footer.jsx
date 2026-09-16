7a5cb862cd74fa24f425a29a95c76fdbbfa14f91
Madhav Sharma <madhav032001@gmail.com>
2026-09-17 00:49:29 +0530

    form added

--- my-fylo-project/src/Pages/Footer.jsx
+++ my-fylo-project/src/Pages/Footer.jsx
@@ -1,8 +1,9 @@
 export default function Footer() {
   return (
     <div className="bg-[#0b1524] w-full h-100 mt-[-30vh]">
-      <div className="text-left text-white  pt-[38vh] pl-15">
-        +2-543-123-4567
+      <div className="text-left text-[1.3vw] text-white  pt-[38vh] pl-15">
+        <h1>+2-543-123-4567</h1>
+        <h1 className="pt-5"> example@fylo.com </h1>
       </div>
     </div>
   );
